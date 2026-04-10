83009dbbaa7213deb2bc903312577bb70cb88073
Victor <vmp134@scarletmail.rutgers.edu>
2026-04-09 20:18:22 -0400

    Added makefile, updated mysh.c and mysh.h

---
+++ mysh.c
@@ -0,0 +1,30 @@
+#include "mysh.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Parsing commands
+int parser() {
+
+}
+
+//Main
+int main(int argc, char **argv) {
+    int inputFD = 0;
+    int isInteractive = 0;
+
+    //Input 
+
+
+    //Parsing Loop
+
+}
