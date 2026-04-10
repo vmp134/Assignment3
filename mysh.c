bd5fcc1e56b11683ff3314fed589b6f51161cf95
Victor <vmp134@scarletmail.rutgers.edu>
2026-04-09 20:53:30 -0400

    Updated main() in mysh.c

--- mysh.c
+++ mysh.c
@@ -1,17 +1,5 @@
 #include "mysh.h"
 
-
-
-
-
-
-
-
-
-
-
-
-
 //Parsing commands
 int parser() {
 
@@ -19,12 +7,34 @@ int parser() {
 
 //Main
 int main(int argc, char **argv) {
+    //Flags for our parsing loop
     int inputFD = 0;
     int isInteractive = 0;
 
-    //Input 
+    //Checking flags
+    /*
+        If argc == 2, always run batch mode
+            If open() returns -1, the file failed to open
+        If there is no arg, check stdin with isatty()
+        If too many args, return fail
+    */
+    if (argc == 2) {
+        inputFD = open(argv[1], O_RDONLY);
+        if (inputFD < 0) {
+            perror("Error opening batch file");
+            return EXIT_FAILURE;
+        }
+        isInteractive = 0;
+    }
+    else if (argc == 1){
+        if (isatty(STDIN_FILENO)) {
+            isInteractive = 1;
+        }
+        else isInteractive = 0;
+    }
+    else return EXIT_FAILURE;
 
-
-    //Parsing Loop
+    //Parsing loop
+    
 
 }
